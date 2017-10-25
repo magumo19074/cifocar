311cdf2b8a15120fa38d82dec66f814ff5fd103a
gloria <gloriapujolc@gmail.com>
2017-10-25 13:32:18 +0200

    Modifcocion Template - tilulo portada

--- cifocar/templates/Template.php
+++ cifocar/templates/Template.php
@@ -10,8 +10,8 @@
 					</a>
 				</figure>
 				<hgroup>
-					<h1>RMF - RobS Micro Framework</h1>
-					<h2>Para el desarrollo de aplicaciones web</h2>
+					<h1>CIFOCAR Valles</h1>
+					<h2>Concesionario de cohes de seguna mano</h2>
 				</hgroup>
 			</header>
 		<?php }
