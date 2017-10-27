be1e9eb44a4f5dfb2d6f2772b171e9faaa69c771
magumo <magumo19074@gmail.com>
2017-10-27 13:33:53 +0200

    vehiculomodel 2

--- cifocar/model/VehiculoModel.php
+++ cifocar/model/VehiculoModel.php
@@ -1,5 +1,5 @@
 <?php
-//definición de la clase Juegos
+//definición de la clase VEHICULOS
 class VehiculoModel{
     //propiedades "¿qué tienen?"
     public $id, $matricula, $detalles, $modelo,$precio_venta,$imagen,$precio_compra,$kms,$caballos;
@@ -12,9 +12,23 @@ class VehiculoModel{
         $conexion = Database::get();
         
         //preparar la consulta de inserción
-        $consulta = "INSERT INTO vehiculos (id, modelo, matricula, detalles,precio_venta,imagen,precio_compra,kms,caballos,fecha_venta,estado,any_matriculacion,vendedor,marca,color)
-                         VALUES(DEFAULT, '$this->modelo', '$this->matricula', '$this->detalles','$this->precio_venta','$this->imagen','$this->precio_compra','$this->kms','$this->caballos','$this->fecha_venta','$this->estado','$this->any_matriculacion','$this->vendedor','$this->marca','$this->color');";
-        
+        $consulta = "INSERT INTO vehiculos (modelo, matricula, detalles,precio_venta,imagen,precio_compra,kms,caballos,fecha_venta,estado,any_matriculacion,vendedor,marca,color)
+                    VALUES (
+                    '$this->modelo', 
+                    '$this->matricula', 
+                     '$this->detalles',
+                    $this->precio_venta,
+                    '$this->imagen',
+                    $this->precio_compra,
+                    $this->kms,
+                    $this->caballos,
+                    '$this->fecha_venta',
+                    $this->estado,
+                    $this->any_matriculacion,
+                    $this->vendedor,
+                    '$this->marca',
+                    '$this->color');";
+        var_dump($consulta);
         //lanzar la consulta
         return $conexion->query($consulta);
     }
