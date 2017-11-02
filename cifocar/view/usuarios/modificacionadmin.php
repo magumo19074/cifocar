db38d20df0001f688db702285bb9b41053538613
magumo <magumo19074@gmail.com>
2017-11-02 10:00:59 +0100

    modificacion admin

--- cifocar/view/usuarios/modificacionadmin.php
+++ cifocar/view/usuarios/modificacionadmin.php
@@ -23,42 +23,43 @@
 			
 			<h2>Formulario de modificación de datos</h2>
 			
+			<div class="contenedor">
+    			<article class="texto3">
+			
 			<form method="post" enctype="multipart/form-data" autocomplete="off">
 				
-				<figure>
-					<img class="imagenactual" src="<?php echo $usuarios->imagen;?>" 
-						alt="<?php echo  $usuarios->user;?>" />
-				</figure>
 				
-				
-				
-				
-				
-				
-				<label>Email:</label>
+				<h2>Datos a modificar</h2>
+			    <label>Email:</label>
 				<input type="email" name="email" required="required" 
 					value="<?php echo $usuarios->email;?>"/><br/>
 					
 				<label>Administrador</label>
-				<input type="checkbox" name="admin" value="1">Administrador</br>
-				</br>
+				<input type="checkbox" name="admin" <?php if($usuarios->admin) echo "checked='checked'"?>>Administrador<br/>
+				<br/>
 					
 				<label>Privilegio:</label>
-					<select name="privilegio">
-						<option value="0">Administrador</option>
-						<option value="1">Responsable de compras</option>
-						<option value="2" selected="selected">Vendedor</option>
-					</select>
-				<br/>
+				<select name="privilegio">
+    					<option value="0" <?php if($usuarios->privilegio==0) echo "selected='selected'";?>>Administrador</option>
+    					<option value="1" <?php if($usuarios->privilegio==1) echo "selected='selected'";?>>Responsable de compras</option>
+    					<option value="2" <?php if($usuarios->privilegio==2) echo "selected='selected'";?>>Vendedor</option>
+    			</select><br/>
+					
 				
 				<label>Nueva imagen:</label>
 				<input type="hidden" name="MAX_FILE_SIZE" value="<?php echo $max_image_size;?>" />		
 				<input type="file" accept="image/*" name="imagen" />
 				<br/>
 				
-				<label></label>
-				<input type="submit" name="modificar" value="modificar"/><br/>
-			</form>
+				</article>
+				
+				<figure class="imagen">
+					<img src="<?php echo $usuarios->imagen;?>" 
+						alt="<?php echo  $usuarios->user;?>" />
+				</figure>
+				</div>
+				<input style="margin-left: 300px" class="botonok2" type="submit" name="modificar" value="modificar"/><br/>
+		</form>
 			
 				
 		</section>
